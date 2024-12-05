0163f910c78032aa621cbbf1de099a705aa7a3bb
hzrg <zrhoua@163.com>
2024-12-05 22:40:59 +0800

    first commit

---
+++ netflix/spring-cloud-ribbon-demo/src/test/java/org/zrhou/demo/springcloudribbondemo/SpringCloudRibbonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zrhou.demo.springcloudribbondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRibbonDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
